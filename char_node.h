fcaa8f3a1e047c4f23ddb4cd35541687d0bfbbf7
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-11-02 15:36:54 +0100

    Update char_node.h

--- char_node.h
+++ char_node.h
@@ -1,38 +1,6 @@
 #ifndef UNTITLED_CHAR_NODE_H
 #define UNTITLED_CHAR_NODE_H
 
-// Sous-type des formes fléchis
-typedef enum
-{
-    // Genre (Masculin / Féminin - ADJ/NOM)
-    Mas, Fem,
-    // Nombre (Singulier / Pluriel - ADJ/NOM/VER)
-    SG, PL,
-    // Personne (1e 2e 3e personne - VER)
-    P1, P2, P3,
-
-    // Verbe
-    // (Infinitif)
-    Inf,
-    // (Participe passé / Participe Présent)
-    PPas, PPre,
-    // (Indicatif : Présent / Passé simple / Imparfait / Futur)
-    IPre, IPSim, IImp, IFut,
-    // (Subjonctif : Présent / Passé (??) / Imparfait / Plus que parfait (??)) |
-    SPre, SImp,
-    // (Conditionnel : Présent)
-    CPre,
-    // Impératif (Présent / Passé(x)) | Pas d'impératif passé
-    Imp, ImPre,
-
-    // Nom
-    Inv,
-
-    // Adjectif
-    InvGen,
-}sub_type;
-
-/*
 struct s_char_node
 {
     char value;

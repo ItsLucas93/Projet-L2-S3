ccf2e02e78ec284e15fbb473418615e39e9f977f
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-11-13 21:52:41 +0100

    Update flechie_node.c

--- flechie_node.c
+++ flechie_node.c
@@ -8,7 +8,7 @@ p_flechie_node createFlechieNode()
     p_flechie_node nouv = (p_flechie_node) malloc (sizeof (t_flechie_list));
 
     nouv->next = NULL;
-    nouv->sub_type_list = createEmptyEnumQueue();
+    nouv->sub_type_list->head = NULL;
     // char value[LENGHT_MAX]; // mot sous forme fléchie
 
     return nouv;

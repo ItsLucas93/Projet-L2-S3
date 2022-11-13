2bc8a9a2e1185402de4f2b5c4dc1f7f5fd48aa0e
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-11-13 17:58:46 +0100

    Add function isFlechieInList

--- enum_list.c
+++ enum_list.c
@@ -5,6 +5,17 @@
 t_enum_list createEmptyEnumQueue()
 {
     t_enum_list t;
-    t.head = t.tail = NULL;
+    t.head = NULL;
     return t;
+}
+
+int isFlechieInList(t_enum_list t, sub_type sous_type)
+{
+    p_enum_node temp = t.head;
+    while (temp != NULL && temp->value != sous_type)
+    {
+        temp = temp->next;
+    }
+    if (temp != NULL && temp->value == sous_type) return 1;
+    else return 0;
 }
--- enum_list.h
+++ enum_list.h
@@ -8,8 +8,9 @@ struct s_enum_list
     struct s_enum_node *head;
 };
 
-typedef struct s_enum_list t_enum_list, *p_enum_list;
+typedef struct s_enum_list s_enum_list, t_enum_list, *p_enum_list;
 
 t_enum_list createEmptyEnumQueue();
+int isFlechieInList(t_enum_list, sub_type);
 
 #endif //PROJET_ENUM_LIST_H

37afce1e3125ff129c37f82045033c09efdc091c
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-11-16 17:49:16 +0100

    Update base_tree.c

--- base_tree.c
+++ base_tree.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 
 #include "base_tree.h"
+#include <stdio.h>
 
-t_base_tree createEmptyTree()
+t_base_tree createEmptyBaseTree()
 {
 	t_base_tree t;
+    printf("%d", t.root[0]->value);
+    for (int i = 0 ; i < ALPHABET_SIZE ; i++)
+    {
+        t.root[i] = NULL;
+    }
 	return t;
 }
 
@@ -13,12 +19,22 @@ p_base_node insertBaseTree(p_base_tree t, char* val)
 {
     // ascii a 97 to 122
     unsigned long lenght_word = strlen(val);
-    p_base_node temp = t->root[((int)val[0] - 97)];
-    for (unsigned long i = 0 ; i < lenght_word ; i++)
-    {
-        temp->value = val[i];
-        temp->fils[i] = createBaseNode();
-        temp = temp->fils[i];
+
+    if (lenght_word) {
+        t->root[((int) val[0] - 97)] = createBaseNode();
+        t->root[((int) val[0] - 97)]->value = val[0];
+
+        p_base_node temp = t->root[((int) val[0] - 97)];
+        // lenght_word - 1 pour ne pas prendre en compte le \0
+        for (unsigned long i = 0; i < lenght_word - 1; i++)
+        {
+            int idx = ((int) val[i + 1] - 97);
+            temp->fils[idx] = createBaseNode();
+            temp = temp->fils[idx];
+            temp->value = val[idx];
+        }
+
+        return temp;
     }
-    return temp;
+    return NULL;
 }

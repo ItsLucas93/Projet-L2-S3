44e9ad4bc5e5b726d50827f79c660a41e5e08d3a
ELisee13 <104190568+ELisee13@users.noreply.github.com>
2022-11-16 19:30:34 +0100

    Update enum_node.c

--- enum_node.c
+++ enum_node.c
@@ -13,3 +13,10 @@ p_enum_node createEnumNode(sub_type val)
 
     return nouv;
 }
+
+p_char_type create_list_type(){
+    p_char_type  type_list;
+    type_list = (p_char_type)malloc(sizeof(t_char_type));
+    type_list->next=NULL;
+    return type_list;
+}
